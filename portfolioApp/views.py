1fbc5619d59c60af61543b4c9cb7428f1cf2c6df
Kevin-Kibunyi <42345139+Kevin-Kibunyi@users.noreply.github.com>
2022-10-26 07:46:53 +0300

    Edit User Profile

--- portfolioApp/views.py
+++ portfolioApp/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
-from .forms import EditUserProfileForm
 
 
 # Create your views here.
@@ -12,17 +9,5 @@ def index(request):
 
 
 def edit_userprofile(request):
-    if request.method == 'POST':
-        form = EditUserProfileForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'You have Edited Your Profile Successfully...')
-            return redirect('index')
-
-    else:
-        form = EditUserProfileForm(instance=request.user)
-    context = {'form': form}
-    return render(request, 'edit_userprofile.html', context)
-
-
+    return render(request, 'edit_userprofile.html')
 
